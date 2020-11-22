d42b1a44bda6a006f5086a093d0f60cfce94a649
pranalideogade <pran2871@gmail.com>
2020-11-21 21:08:50 -0500

    update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          welcome pran
+          welcome pran updated
         </a>
       </header>
     </div>
